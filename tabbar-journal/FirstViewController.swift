6ad14d4764126a6cbc1071278fcea73f2d84ae68
Joel Shin <jwshin@gmail.com>
2017-01-22 15:54:37 -0800

    Initial Commit

---
+++ tabbar-journal/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  tabbar-journal
+//
+//  Created by Joel Shin on 1/22/17.
+//  Copyright © 2017 Joel Shin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
