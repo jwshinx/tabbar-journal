35a62809dd952919091eef1bc923cbc004a9a08a
Joel Shin <jwshin@gmail.com>
2017-01-26 18:40:41 -0800

    first controller shows date and name onbuttonclick

--- tabbar-journal/FirstViewController.swift
+++ tabbar-journal/FirstViewController.swift
@@ -10,6 +10,64 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBAction func onButtonClick(sender: AnyObject) {
+        print("+++> onButtonClick")
+        let now = NSDate()
+        NSUserDefaults.standardUserDefaults().setObject(now, forKey: "buttonTapped")
+        NSUserDefaults.standardUserDefaults().setObject("Joel Shin", forKey: "name")
+        self.updateDateLabel()
+        self.updateNameLabel()
+
+    }
+    
+    func updateNameLabel() {
+        let fullName = NSUserDefaults.standardUserDefaults().objectForKey("name") as? NSString
+        if let hasName = fullName {
+            self.nameLabel.text = hasName as String
+        } else {
+            self.nameLabel.text = "No name info."
+        }
+    }
+    
+    func updateDateLabel() {
+        let lastUpdate = NSUserDefaults.standardUserDefaults().objectForKey("buttonTapped") as? NSDate
+        let formatter:NSDateFormatter = NSDateFormatter()
+        formatter.dateFormat = "MMM d, yyyy"
+
+        if let hasLastUpdate = lastUpdate {
+            self.dateLabel.text = formatter.stringFromDate(hasLastUpdate)
+        } else {
+            self.dateLabel.text = "No date saved."
+        }
+    }
+
+
+/*
+func updateAuthorLabel() {
+    let author = NSUserDefaults.standardUserDefaults().objectForKey("author") as? NSString
+    print("+++> author: \(author!)")
+    if let hasAuthor = author {
+        self.nameLabel.text = hasAuthor as String
+    } else {
+        self.nameLabel.text = "No author info."
+    }
+}
+
+func updateDateLabel() {
+    let lastUpdate = NSUserDefaults.standardUserDefaults().objectForKey("buttonTapped") as? NSDate
+    if let hasLastUpdate = lastUpdate {
+        self.dateLabel.text = hasLastUpdate.description
+    } else {
+        self.dateLabel.text = "No date saved."
+    }
+}
+*/
+
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
