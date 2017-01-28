9364d32a0aa79d5c5fcf33c85e57b61508d5f910
Joel Shin <jwshin@gmail.com>
2017-01-28 10:03:26 -0800

    cleaned up previous commit work - NSUrlSession

--- tabbar-journal/PhotosTableViewController.swift
+++ tabbar-journal/PhotosTableViewController.swift
@@ -40,45 +40,45 @@ class PhotosTableViewController : UITableViewController {
     }
     
     override func viewDidLoad() {
-        print("+++> AD.viewDidLoad start")
+        print("+++> PTVC.viewDidLoad start")
         
         // JWS it calls updateFeed and passes in "completion" block to run after done.
         //   it finds IFTVC and see that when feed called, it calls reloadData
         
         let italyUrlString = "https://api.flickr.com/services/feeds/photos_public.gne?tags=pantheon&format=json&nojsoncallback=1"
-        print("+++> AD italyUrlString: \(italyUrlString)")
+        print("+++> PTVC italyUrlString: \(italyUrlString)")
         // if let url = NSURL(string: foundURLString) {
         if let url = NSURL(string: italyUrlString) {
-            print("+++> AD lets call updateFeed: \(url)")
+            print("+++> PTVC lets call updateFeed: \(url)")
             
             self.updateFeed(url, completion: { (feed) -> Void in
                 // let viewController = application.windows[0].rootViewController as? PhotosTableViewController
-                print("+++> AD running completion block \(self)")
+                print("+++> PTVC running completion block \(self)")
                 // JWS when iftvc.feed set, reloadData happens. see iftvc
                 self.feed = feed
             })
         }
-        print("+++> AD.viewDidLoad end")
+        print("+++> PTVC.viewDidLoad end")
     }
     
     // JWS loads feed and feeditems via api url
     // after feed item loaded, completion block is called
     func updateFeed(url: NSURL, completion: (feed: Feed?) -> Void) {
-        print("+++> AD.updateFeed start url: \(url)")
+        print("+++> PTVC.updateFeed start url: \(url)")
         let request = NSURLRequest(URL: url)
         let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { (data, response, error) -> Void in
-            print("+++> AD.updateFeed response: \(response)")
+            print("+++> PTVC.updateFeed response: \(response)")
             if error == nil && data != nil {
                 let feed = Feed(data: data!, sourceURL: url)
                 NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
-                    print("+++> AD.updateFeed lets call completion")
+                    print("+++> PTVC.updateFeed lets call completion")
                     completion(feed: feed)
                 })
             }
         }
         
         task.resume()
-        print("+++> AD.updateFeed end")
+        print("+++> PTVC.updateFeed end")
     }
     
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
