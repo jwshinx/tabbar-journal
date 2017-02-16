2219d4c2dc6d174bf040082609d59651292c693b
Joel Shin <jwshin@gmail.com>
2017-02-16 02:34:13 -0800

    rfr

--- tabbar-journal/BandsTableViewController.swift
+++ tabbar-journal/BandsTableViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class BandsTableViewController: UITableViewController {
     
-    var bands:[String] = ["Radiohead", "Nirvana", "Beatles", "Air"]
     var artists:[Band] = []
 
     override func viewDidLoad() {
@@ -20,40 +19,32 @@ class BandsTableViewController: UITableViewController {
 
         let radiohead: Band = Band(nameAndHomeTownAndBirthDate: "Radiohead", homeTown: "Leeds", birthDate: NSDate());
         let beatles: Band = Band(nameAndHomeTownAndBirthDate: "Beatles", homeTown: "Liverpool", birthDate: NSDate());
-        let arcadeFire: Band = Band(name: "Arcade Fire")
         let u2: Band = Band(nameAndHomeTown: "U2", homeTown: "Dublin");
         let rem: Band = Band(nameAndHomeTownAndBirthDate: "REM", homeTown: "Athens", birthDate: NSDate());
 
         artists.append(rem)
         artists.append(radiohead)
         artists.append(beatles)
-        artists.append(arcadeFire)
         artists.append(u2)
         
         let theBends: Album = Album(titleAndYearAndRevenue: "The Bends", year: 1995, revenue: 11)
         let okComputer: Album = Album(titleAndYearAndRevenue: "Ok Computer", year: 1997, revenue: 204)
         let amnesiac: Album = Album(titleAndYearAndRevenue: "Amnesiac", year: 2003, revenue: 224)
         let inRainbows: Album = Album(titleAndYearAndRevenue: "In Rainbows", year: 2005, revenue: 91)
-        
-        radiohead.addAlbum(theBends)
-        radiohead.addAlbum(okComputer)
-        radiohead.addAlbum(amnesiac)
-        radiohead.addAlbum(inRainbows)
+        populateDiscography(radiohead, albums: [theBends, okComputer, amnesiac, inRainbows])
 
         let sp: Album = Album(titleAndYearAndRevenue: "Sargent Peppers", year: 1967, revenue: 67)
         let revolver: Album = Album(titleAndYearAndRevenue: "Revolver", year: 1965, revenue: 81)
         let wa: Album = Album(titleAndYearAndRevenue: "White Album", year: 1962, revenue: 103)
+        populateDiscography(beatles, albums: [sp, revolver, wa])
         
-        beatles.addAlbum(sp)
-        beatles.addAlbum(revolver)
-        beatles.addAlbum(wa)
-        
-        populateDiscography(rem)
+        let green: Album = Album(titleAndYearAndRevenue: "Green", year: 1988, revenue: 75)
+        let document: Album = Album(titleAndYearAndRevenue: "Document", year: 1985, revenue: 8)
+        populateDiscography(rem, albums: [green, document])
         
         let joshuaTree: Album = Album(titleAndYearAndRevenue: "Joshua Tree", year: 1988, revenue: 112)
         let war: Album = Album(titleAndYearAndRevenue: "War", year: 1983, revenue: 35)
-        u2.addAlbum(joshuaTree)
-        u2.addAlbum(war)
+        populateDiscography(u2, albums: [joshuaTree, war])
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -62,6 +53,12 @@ class BandsTableViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
+    func populateDiscography(beatles: Band, albums: [Album]) {
+        for album in albums {
+            beatles.addAlbum(album)
+        }
+    }
+    /*
     func populateDiscography(rem: Band) {
         let green: Album = Album(titleAndYearAndRevenue: "Green", year: 1988, revenue: 75)
         let document: Album = Album(titleAndYearAndRevenue: "Document", year: 1985, revenue: 8)
@@ -78,7 +75,7 @@ class BandsTableViewController: UITableViewController {
                 print("------>     ***> \(album)")
             }
         }
-    }
+    }*/
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
