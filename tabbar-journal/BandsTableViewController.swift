c478c2dfc5d5e7b919fc002cc971617bee693970
Joel Shin <jwshin@gmail.com>
2017-02-16 02:08:28 -0800

    joshua tree

--- tabbar-journal/BandsTableViewController.swift
+++ tabbar-journal/BandsTableViewController.swift
@@ -21,31 +21,39 @@ class BandsTableViewController: UITableViewController {
         let radiohead: Band = Band(nameAndHomeTownAndBirthDate: "Radiohead", homeTown: "Leeds", birthDate: NSDate());
         let beatles: Band = Band(nameAndHomeTownAndBirthDate: "Beatles", homeTown: "Liverpool", birthDate: NSDate());
         let arcadeFire: Band = Band(name: "Arcade Fire")
-        let stoneRoses: Band = Band(nameAndHomeTown: "Stone Roses", homeTown: "Manchester");
+        let u2: Band = Band(nameAndHomeTown: "U2", homeTown: "Dublin");
         let rem: Band = Band(nameAndHomeTownAndBirthDate: "REM", homeTown: "Athens", birthDate: NSDate());
 
         artists.append(rem)
         artists.append(radiohead)
         artists.append(beatles)
         artists.append(arcadeFire)
-        artists.append(stoneRoses)
+        artists.append(u2)
         
         let theBends: Album = Album(titleAndYearAndRevenue: "The Bends", year: 1995, revenue: 11)
         let okComputer: Album = Album(titleAndYearAndRevenue: "Ok Computer", year: 1997, revenue: 204)
         let amnesiac: Album = Album(titleAndYearAndRevenue: "Amnesiac", year: 2003, revenue: 224)
         let inRainbows: Album = Album(titleAndYearAndRevenue: "In Rainbows", year: 2005, revenue: 91)
+        
         radiohead.addAlbum(theBends)
         radiohead.addAlbum(okComputer)
         radiohead.addAlbum(amnesiac)
         radiohead.addAlbum(inRainbows)
 
+        let sp: Album = Album(titleAndYearAndRevenue: "Sargent Peppers", year: 1967, revenue: 67)
+        let revolver: Album = Album(titleAndYearAndRevenue: "Revolver", year: 1965, revenue: 81)
+        let wa: Album = Album(titleAndYearAndRevenue: "White Album", year: 1962, revenue: 103)
+        
+        beatles.addAlbum(sp)
+        beatles.addAlbum(revolver)
+        beatles.addAlbum(wa)
+        
         populateDiscography(rem)
         
-        /*
-        let joshuaTree: Album = Album(titleAndYear: "Joshua Tree", year: 1985)
-        joshuaTree.revenue = 112
-        print("+++ +++> album: \(joshuaTree)")
-        */
+        let joshuaTree: Album = Album(titleAndYearAndRevenue: "Joshua Tree", year: 1988, revenue: 112)
+        let war: Album = Album(titleAndYearAndRevenue: "War", year: 1983, revenue: 35)
+        u2.addAlbum(joshuaTree)
+        u2.addAlbum(war)
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
