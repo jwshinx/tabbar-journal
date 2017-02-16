e535d73c1236b17fc0d678c94af17a89f5d54bf2
Joel Shin <jwshin@gmail.com>
2017-02-16 02:40:35 -0800

    rfr

--- tabbar-journal/BandsTableViewController.swift
+++ tabbar-journal/BandsTableViewController.swift
@@ -22,40 +22,44 @@ class BandsTableViewController: UITableViewController {
         let u2: Band = Band(nameAndHomeTown: "U2", homeTown: "Dublin");
         let rem: Band = Band(nameAndHomeTownAndBirthDate: "REM", homeTown: "Athens", birthDate: NSDate());
 
-        artists.append(rem)
         artists.append(radiohead)
         artists.append(beatles)
         artists.append(u2)
+        artists.append(rem)
         
-        let theBends: Album = Album(titleAndYearAndRevenue: "The Bends", year: 1995, revenue: 11)
-        let okComputer: Album = Album(titleAndYearAndRevenue: "Ok Computer", year: 1997, revenue: 204)
-        let amnesiac: Album = Album(titleAndYearAndRevenue: "Amnesiac", year: 2003, revenue: 224)
-        let inRainbows: Album = Album(titleAndYearAndRevenue: "In Rainbows", year: 2005, revenue: 91)
-        populateDiscography(radiohead, albums: [theBends, okComputer, amnesiac, inRainbows])
-
-        let sp: Album = Album(titleAndYearAndRevenue: "Sargent Peppers", year: 1967, revenue: 67)
-        let revolver: Album = Album(titleAndYearAndRevenue: "Revolver", year: 1965, revenue: 81)
-        let wa: Album = Album(titleAndYearAndRevenue: "White Album", year: 1962, revenue: 103)
-        populateDiscography(beatles, albums: [sp, revolver, wa])
+        initializeAlbums()
         
-        let green: Album = Album(titleAndYearAndRevenue: "Green", year: 1988, revenue: 75)
-        let document: Album = Album(titleAndYearAndRevenue: "Document", year: 1985, revenue: 8)
-        populateDiscography(rem, albums: [green, document])
-        
-        let joshuaTree: Album = Album(titleAndYearAndRevenue: "Joshua Tree", year: 1988, revenue: 112)
-        let war: Album = Album(titleAndYearAndRevenue: "War", year: 1983, revenue: 35)
-        populateDiscography(u2, albums: [joshuaTree, war])
-
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
+    
+    func initializeAlbums() {
+        let theBends: Album = Album(titleAndYearAndRevenue: "The Bends", year: 1995, revenue: 11)
+        let okComputer: Album = Album(titleAndYearAndRevenue: "Ok Computer", year: 1997, revenue: 204)
+        let amnesiac: Album = Album(titleAndYearAndRevenue: "Amnesiac", year: 2003, revenue: 224)
+        let inRainbows: Album = Album(titleAndYearAndRevenue: "In Rainbows", year: 2005, revenue: 91)
+        populateDiscography(artists[0], albums: [theBends, okComputer, amnesiac, inRainbows])
+        
+        let sp: Album = Album(titleAndYearAndRevenue: "Sargent Peppers", year: 1967, revenue: 67)
+        let revolver: Album = Album(titleAndYearAndRevenue: "Revolver", year: 1965, revenue: 81)
+        let wa: Album = Album(titleAndYearAndRevenue: "White Album", year: 1962, revenue: 103)
+        populateDiscography(artists[1], albums: [sp, revolver, wa])
 
-    func populateDiscography(beatles: Band, albums: [Album]) {
+        let joshuaTree: Album = Album(titleAndYearAndRevenue: "Joshua Tree", year: 1988, revenue: 112)
+        let war: Album = Album(titleAndYearAndRevenue: "War", year: 1983, revenue: 35)
+        populateDiscography(artists[2], albums: [joshuaTree, war])
+
+        let green: Album = Album(titleAndYearAndRevenue: "Green", year: 1988, revenue: 75)
+        let document: Album = Album(titleAndYearAndRevenue: "Document", year: 1985, revenue: 8)
+        populateDiscography(artists[3], albums: [green, document])
+    }
+
+    func populateDiscography(band: Band, albums: [Album]) {
         for album in albums {
-            beatles.addAlbum(album)
+            band.addAlbum(album)
         }
     }
     /*
